6abe9459ef20db90d6ad1cd176ed1e6524b612d2
Martin <martinagnalv@gmail.com>
2017-05-18 15:52:38 +1000

    updated code

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 module.exports = function possessive(name: string, locale: string) {
-  var apostrophe
+  var apostrophe = ''
+  console.warn(locale)
   switch (locale) {
     case 'da':
       apostrophe = ''
+      break
     case 'fi':
       apostrophe = ''
+      break
     case 'is':
       apostrophe = ''
+      break
     case 'sv':
       apostrophe = ''
+      break
     case 'nb':
       apostrophe = ''
+      break
     case 'en':
       apostrophe = '’'
+      break
     case 'fr':
       apostrophe = ''
+      break
     case 'nl':
       apostrophe = ''
+      break
     case 'be':
       apostrophe = ''
+      break
     case 'it':
       apostrophe = ''
+      break
     case 'es':
       apostrophe = ''
+      break
     case 'de':
       apostrophe = ''
+      break
     case 'et':
       apostrophe = ''
-    default: ''
+      break
+    default: apostrophe = ''
 
   }
-  if(name == '') {
-    return name;
+  if (name == '') {
+    return name
   }
   var lastChar = name.slice(-1);
   var endOfWord = lastChar.toLowerCase() == 's' ? apostrophe : `${apostrophe}s`;
